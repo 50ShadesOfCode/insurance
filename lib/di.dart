fcc2fcbba7dd045cc9c7299430ecd85ff9e96ebd
50ShadesOfCode <kaktymail@gmail.com>
2023-05-22 23:24:41 +0300

    Add prefs provider to di.

--- lib/di.dart
+++ lib/di.dart
@@ -1,9 +1,20 @@
 import 'package:get_it/get_it.dart';
+import 'package:insurance/providers/shared_prefs_provider.dart';
 
 final GetIt appLocator = GetIt.instance();
 
 final AppDi appDi = AppDi();
 
 class AppDi {
-  Future<void> initDependencies() async {}
+  Future<void> initDependencies() async {
+    initPrefs();
+  }
+
+  Future<void> initPrefs() async {
+    final SharedPrefsProvider prefsProvider = SharedPrefsProvider();
+    await Future.wait(
+        <Future<void>>[prefsProvider.initializeSharedPreferences()]);
+
+    appLocator.registerSingleton<SharedPrefsProvider>(prefsProvider);
+  }
 }
